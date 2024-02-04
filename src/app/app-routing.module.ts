8109c737c175b9c7527a4dd42691d356096a8f83
DMorozco <dianaorozco0830@gmail.com>
2024-02-04 14:13:13 +0100

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonViewComponent } from './pokemon-view/pokemon-view.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PokemonListComponent },
+  {path: 'view/:pokemonname', component: PokemonViewComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
